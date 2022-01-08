bbb77f7464d5327d5e25d74ca6ec3d4403e084d6
Izrael <israelabaho@gmail.com>
2022-01-07 21:34:21 -0800

    Starter File

---
+++ main.js
@@ -0,0 +1,29 @@
+// All dom elements ==========================================>
+
+
+// Add itmes to cart Functionality ==============================>
+
+
+// Function to add Menu
+
+
+// Show Cart Screen ====================================>
+
+
+// Remove navBar by each navLink
+
+
+// adding shadow to the header=======================
+
+
+// Change Theme Function===================================
+
+
+// Home Section swiper function ========================>
+
+
+ //Featured Section Function =======================>
+
+
+  
+  
